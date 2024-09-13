90959ea762a534edecb3513f193b68aebaaa07c6
Trần Đình Tân <dinhtan131@gmail.com>
2024-09-13 17:52:47 +0700

    Update Sidebar.jsx

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -212,20 +212,6 @@ const Sidebar = () => {
               color={colors.grey[300]}
               sx={{ m: "15px 0 5px 20px" }}
             >
-              Work_log
-            </Typography>
-            <Item
-              title="Manage Work Log"
-              to="/admin/work_log"
-              icon={<Check />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Typography
-              variant="h6"
-              color={colors.grey[300]}
-              sx={{ m: "15px 0 5px 20px" }}
-            >
               Voucher
             </Typography>
             <Item
@@ -249,7 +235,20 @@ const Sidebar = () => {
               selected={selected}
               setSelected={setSelected}
             />
-
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 0 5px 20px" }}
+            >
+              Work_log
+            </Typography>
+            <Item
+              title="Manage Work Log"
+              to="/admin/work_log"
+              icon={<Check />}
+              selected={selected}
+              setSelected={setSelected}
+            />
             <Typography
               variant="h6"
               color={colors.grey[300]}
